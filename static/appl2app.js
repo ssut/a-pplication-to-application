942522ef1c3e92707ab006ad61d99c991bf29898
ssut <ssut@ssut.me>
2015-08-14 20:06:33 +0900

    추가: appl2app

---
+++ static/appl2app.js
@@ -0,0 +1,39 @@
+(function(_) {
+    "use strict";
+
+    var Substitute = function(map) {
+        this.map = map;
+    }
+
+    Substitute.prototype = {
+        /**
+         * Replace values in a string with the corresponding values in the pre-released map.
+         *
+         * @var String text
+         *     a string to be replaced
+         */
+        replace: function(text) {
+            for(let i = 0; i < this.map.length; i++) {
+                // this will be better but chrome can't just yet even ES6:
+                //  let [find, repl] = this.map[i];
+                let find = this.map[i][0];
+                let repl = this.map[i][1];
+                text = text.replace(find, repl);
+            }
+
+            return text;
+        }
+    };
+
+    let replacer = new Substitute([
+        [/어플리케이션/g, '애플리케이션'],
+        [/어플/g, '앱'],
+    ]);
+
+    var appl2app = function(text) {
+        return replacer.replace(text);
+    };
+
+    _.appl2app = appl2app;
+
+})(window);
